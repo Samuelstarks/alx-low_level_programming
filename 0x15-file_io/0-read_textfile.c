159b69d30cab8566d3e67b4ff0624a1631f078ab
Samuelstarks <brandwithsammy@gmail.com>
2023-10-02 15:24:17 -0700

    Updated

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,16 +11,16 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 char *buf;
-ssize_t fd;
+ssize_t ms;
 ssize_t k;
 ssize_t t;
-fd = open(filename, O_RDONLY);
-if (fd == -1)
+ms = open(filename, O_RDONLY);
+if (ms == -1)
 return (0);
 buf = malloc(sizeof(char) * letters);
-t = read(fd, buf, letters);
+t = read(ms, buf, letters);
 k = write(STDOUT_FILENO, buf, t);
 free(buf);
-close(fd);
+close(ms);
 return (k);
 }
